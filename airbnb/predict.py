eb190bb18db4c07020ab6eb148823a649bc15dc8
sapinspys <garri009@fiu.edu>
2019-12-18 20:25:31 -0800

    Added predict.py script, empty

---
+++ airbnb/predict.py
@@ -0,0 +1,5 @@
+# insert pickled model here
+# receive response data from FE here (listing features)
+# predict price with pickled model
+# combine all into one json/dict object
+# return this to app

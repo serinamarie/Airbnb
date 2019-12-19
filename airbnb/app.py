cfd13963558028938b5a7dbca000964d80963a5a
sapinspys <garri009@fiu.edu>
2019-12-18 20:05:56 -0800

    predict endpoint now working

--- airbnb/app.py
+++ airbnb/app.py
@@ -8,13 +8,27 @@ def create_app():
         return 'airbnb predictive project'
 
     @app.route('/predict', methods=['POST'])
-    def predict(name=None):
-            name = name or request.values['user_name']
-            try:
-                if request.method == 'POST':
-                    return name
-            except Exception as e:
-                return 'error'
+    def predict(name=None, message=""):
+        try:
+            content = request.get_json(force=True)
+            
+        except Exception as identifier:
+            message = f'error: {identifier}'
+        return message
     
     return app
 
+# def tokenize(text):
+#     return [
+#         token for token in simple_preprocess(text) if token not in STOPWORDS
+#         ]
+# @app.route('/', methods=['POST', 'GET'])
+# def responses():
+#     response = requests.get(
+#         'backendurlhere')
+#     return str(response.text)
+# @app.route('/api', methods=['POST', 'GET'])
+# def predict():
+#     data = request.get_json(force=True)
+#     desc = data['feature'][0]['description']
+#     description_length = len(tokenize(desc))
